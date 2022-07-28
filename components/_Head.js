6a7019b6c38f89c65b909065175b2ceb8c7563c2
Chris Read <chrisread@Chriss-MacBook-Pro.local>
2022-07-28 14:15:11 -0500

    add components for Head and footer

---
+++ components/_Head.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function _Head() {
+  return (
+    <Head>
+      <title>PAIRA</title>
+      <meta name="description" content="Welcome to PAIRA!" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
