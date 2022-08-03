575ddd09868f398a245ba1e32328e72f04ee0a1a
Chris Read <24418909+rcreadii@users.noreply.github.com>
2022-08-03 00:04:23 -0600

    add show up signup modal + comment out login dynamic data

--- pages/login.js
+++ pages/login.js
@@ -1,84 +1,121 @@
+import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Login.module.css";
 import _Head from "../components/_Head.js";
 import Footer from "../components/Footer.js";
-import FormControl from "@mui/material/FormControl";
-import Input from "@mui/material/Input";
-import TextField from "@mui/material/TextField";
+import Header from "../components/Header.js";
 import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal"
-import Divider from "@mui/material/Divider"
-import { useState } from 'react';
+import Divider from "@mui/material/Divider";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import { useEffect, useState } from "react";
 
 export default function Login() {
   const handleSubmit = async (event) => {
     // Stop the form from submitting and refreshing the page.
-    event.preventDefault();
+    // event.preventDefault();
 
-    const data = {
-      firstName: event.target.first.value,
-      lastName: event.target.last.value,
-    };
+    // const data = {
+    //   firstName: event.target.first.value,
+    //   lastName: event.target.last.value,
+    // };
 
-    console.log(data);
+    // console.log(data);
 
-    const JSONdata = JSON.stringify(data);
+    // const JSONdata = JSON.stringify(data);
 
-    const endpoint = "/api/hello";
+    // const endpoint = "/api/hello";
 
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
+    // const options = {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSONdata,
+    // };
 
-    const response = await fetch(endpoint, options);
+    // const response = await fetch(endpoint, options);
 
-    const result = await response.json();
+    // const result = await response.json();
 
-    alert(`Is this your full name: ${result.data}`);
+    // alert(`Is this your full name: ${result.data}`);
   };
 
-  const handleClose = async (event) => {
+  // const handleCloseModal = async (event) => {
+  //   setOpen(false);
+  //   return;
+  // };
 
-  };
+  // const handleOpenModal = async (event) => {
+  //   setOpen(true);
+  //   console.log
+  // };
 
-  const handleOpen = async (event) => {
-    setOpen(true);
-    return
-  };
+  const [openModal, setOpenModal] = useState(false);
 
-  const [open, setOpen] = useState(false);
+  const handleModalClose = async (event) => {
+      const modalNode = document.getElementById("modaldiv");
+      const nodeUserClicksOn = event.target;
+      if (!modalNode.contains(nodeUserClicksOn)) {
+        setOpenModal(false);
+      }
+  }
 
   return (
     <div className={styles.container}>
       <_Head />
-      <main
-        className={styles.main}
-      >
+      <main className={styles.main}>
         <div id={styles.div}>
-          <Image
-            alt="paira logo"
-            id={styles.logo}
-            height="50px"
-            src="/../public/paira-logo.png"
-            width="200px"
-          />
+          <Header />
           <p id={styles.createAccount}>
             Create an account to enjoy all the services without any ads free!
           </p>
-          <form action="api/login" id={styles.loginForm} method="post" onSubmit={handleSubmit}>
-            <TextField className={styles.input} id="email" fullWidth placeholder="Email" required type="email"></TextField>
-            <br/>
-            <TextField className={styles.input} id="password" fullWidth placeholder="Password" required type="password"></TextField>
-            <br/>
-            <Button className={styles.button} fullWidth type="submit" size="large" variant="contained">Log In</Button>
+          <form
+            action="api/login"
+            id={styles.loginForm}
+            method="post"
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              className={styles.input}
+              id="email"
+              fullWidth
+              placeholder="Email"
+              required
+              type="email"
+            ></TextField>
+            <br />
+            <TextField
+              className={styles.input}
+              id="password"
+              fullWidth
+              placeholder="Password"
+              required
+              type="password"
+            ></TextField>
+            <br />
+            <Button
+              className={styles.button}
+              fullWidth
+              type="submit"
+              size="large"
+              variant="contained"
+            >
+              Log In
+            </Button>
           </form>
-          <Button style={{margin: "auto", width: "fit-content"}}>Forgot Password?</Button>
-          <Divider style={{margin: "2vh auto", width: "80%"}}></Divider>
-          <Button color="success" onClick={handleOpen} style={{margin: "auto", width: "50%"}} variant="contained">Create new account</Button>
+          <Button style={{ margin: "auto", width: "fit-content" }}>
+            Forgot Password?
+          </Button>
+          <Divider style={{ margin: "2vh auto", width: "80%" }}></Divider>
+          <Button
+            color="success"
+            onClick={() => setOpenModal(true)}
+            style={{ margin: "auto", width: "50%" }}
+            variant="contained"
+          >
+            Create new account
+          </Button>
         </div>
         <Image
           alt="paira student"
@@ -87,16 +124,24 @@ export default function Login() {
           src="/../public/paira-student-girl.png"
           width="100"
         />
-        {/* <Modal open={} onClose={handleClose}>
-          <div>Sign Up</div>
-          <div>Are you a?</div>
-          <div>
-            <div>Parent</div>
-            <div>———— or ————</div>
-            <div>Student</div>
+        <Modal
+          onClick={handleModalClose}
+          open={openModal}
+          style={{ width: "800px", height: "800px" }}
+        >
+          <div id="modaldiv">
+            <div>Signup</div>
+            <Divider style={{ margin: "2vh auto", width: "80%" }}></Divider>
+            <div>Are you a</div>
+            <Link href="/signup/parent">Parent</Link>
+            <Divider style={{ margin: "2vh auto", width: "80%" }}>or</Divider>
+            <Link href="/signup/student">Student</Link>
+            <br />
+            <Link href="/signup/mentor">
+              Interested in working as a mentor?
+            </Link>
           </div>
-          <div>Interested in working as a mentor?</div>
-        </Modal> */}
+        </Modal>
       </main>
       <Footer />
     </div>
