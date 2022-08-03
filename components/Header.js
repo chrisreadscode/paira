1ac21de3d68da6f8dcf57d64424bfbfb6fc5b297
Chris Read <24418909+rcreadii@users.noreply.github.com>
2022-08-03 00:51:26 -0600

    add click header image to go to home page

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,17 @@
-
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
-    return (
-        <div style={{ backgroundColor: "white", width: "100%", margin: "10px" }}>
-            <Image
-                alt="paira logo"
-                height="38px"
-                width="200px"
-                src="/paira-logo.png"
-            />
-        </div>
-    );
+  return (
+    <div style={{ backgroundColor: "white", width: "100%", margin: "10px" }}>
+      <Link href="/">
+        <Image
+          alt="paira logo"
+          height="38px"
+          width="200px"
+          src="/paira-logo.png"
+        />
+      </Link>
+    </div>
+  );
 }
