5d6504b6c926f375be726d4915f73443de27204b
joeypark743 <90875941+joeypark743@users.noreply.github.com>
2022-08-03 10:12:36 -0700

    Profile-student

--- pages/signup/student_profile.js
+++ pages/signup/student_profile.js
@@ -11,28 +11,46 @@ export default function StudentProfile() {
         <><Header />
             <div className={styles.container}>
                 <_Head />
-
-
                 <main className={styles.main}>
 
                     <div id={styles.mainBox}>
                         <Image alt="Background Image"
                             src="/background.jpeg"
                             width="745px"
-                            height="150px"
+                            height="100px"
                             id={styles.background}></Image>
 
-                        <Image alt="Profile Image"
+                        {/* <Image alt="Profile Image"
                             src="/profile_pic.png"
                             width="100px"
                             height="100px"
-                            id={styles.profileImg}></Image>
+                            id={styles.profileImg}></Image> */}
 
                         <div id={styles.profile}>
-                            Paul Sanjeet, 14
+                            <h4 id={styles.title}>Paul Sanjeet, 14</h4>
+                            <h4 style={{ margin: "0px" }}>---</h4>
+                            <p className={styles.fontSmall} style={{ margin: "0px", color: "#9c9c9c" }}>Boston, Massachusetts</p>
+
+                            <p id={styles.education} className={styles.fontLarge}>Education</p>
+                            <div className={styles.textBox} style={{ width: "50%" }}>
+                                <p className={styles.fontSmall} style={{ margin: "0px", color: "#212121" }}>
+                                    Frost High School
+                                </p>
+                            </div>
+
+                            <p id={styles.email} className={styles.fontLarge}>Email</p>
+                            <div className={styles.textBox} style={{ width: "50%" }}>
+                                <p className={styles.fontSmall} style={{ margin: "0px", color: "#212121" }}>
+                                    paulsanjeet@frosths.edu
+                                </p>
+                            </div>
                         </div>
 
                     </div>
+
+                    <div id={styles.sideBox}>
+                    </div>
+
                 </main>
 
                 <Footer />
