50146a4ad551e7e64489f2589e6a06e041aae57d
joeypark743 <90875941+joeypark743@users.noreply.github.com>
2022-08-06 10:26:34 -0700

    Home Page

--- components/HeaderMentor.js
+++ components/HeaderMentor.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import AutoGraphIcon from "@mui/icons-material/AutoGraph";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import HomeIcon from "@mui/icons-material/Home";
 import MessageIcon from "@mui/icons-material/Message";
 import SearchIcon from "@mui/icons-material/Search";
 import styles from "../styles/Header.module.css";
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function Header() {
     return (
@@ -22,49 +22,49 @@ export default function Header() {
             </Link>
 
             <div style={{ display: "flex", flexDirection: "row", float: "right" }}>
-                <Link href="/student/home">
+                <Link href="/mentor/home">
                     <div className={styles.icon}>
                         <HomeIcon className={styles.center} />
                         <div className={styles.center} style={{ fontSize: "small" }}>Home</div>
                     </div>
                 </Link>
 
-                <Link href="/student/insights">
+                <Link href="/mentor/students">
                     <div className={styles.icon}>
-                        <AutoGraphIcon className={styles.center} />
-                        <div className={styles.center} style={{ fontSize: "small" }}>Insights</div>
+                        <PersonIcon className={styles.center} />
+                        <div className={styles.center} style={{ fontSize: "small" }}>Students</div>
                     </div>
                 </Link>
 
-                <Link href="/student/discover">
+                <Link href="/mentor/discover">
                     <div className={styles.icon}>
                         <SearchIcon className={styles.center} />
                         <div className={styles.center} style={{ fontSize: "small" }}>Discover</div>
                     </div>
                 </Link>
 
-                <Link href="/student/schedule">
+                <Link href="/mentor/schedule">
                     <div className={styles.icon}>
                         <CalendarTodayIcon className={styles.center} />
                         <div className={styles.center} style={{ fontSize: "small" }}>Schedule</div>
                     </div>
                 </Link>
 
-                <Link href="/student/message">
+                <Link href="/mentor/message">
                     <div className={styles.icon}>
                         <MessageIcon className={styles.center} />
                         <div className={styles.center} style={{ fontSize: "small" }}>Message</div>
                     </div>
                 </Link>
 
-                <Link href="/student/saved">
+                <Link href="/mentor/saved">
                     <div className={styles.icon}>
                         <BookmarkIcon className={styles.center} />
                         <div className={styles.center} style={{ fontSize: "small" }}>Saved</div>
                     </div>
                 </Link>
 
-                <Link href="/student/profile">
+                <Link href="/mentor/profile">
                     <div className={styles.icon} style={{ opacity: "1", position: "relative", bottom: "7px" }}>
                         <Image alt="Profile Picture"
                             src="/mentor-profile-pic.png"
