13ba87a4d7d1f040da45a9f1e0387a8be21b9618
Chris Read <24418909+rcreadii@users.noreply.github.com>
2022-08-05 23:39:38 -0600

    add sample video, sample reading, and sample blog post pages

---
+++ pages/course-content/sample-video.js
@@ -0,0 +1,18 @@
+import _Head from "../../components/_Head";
+import Footer from "../../components/Footer";
+import HeaderStudent from "../../components/HeaderStudent";
+import styles from "../../styles/Login.module.css";
+import YouTube from "react-youtube";
+
+export default function SampleVideo() {
+  return (
+    <>
+      <_Head />
+      <HeaderStudent />
+      <main className={styles.container + " " + styles.main}>
+        <YouTube videoId={'9FBxfd7DL3E'}/>
+      </main>
+      <Footer />
+    </>
+  );
+}
--- pages/template-page.js
+++ pages/template-page.js
@@ -1,29 +1,19 @@
+import _Head from "../components/_Head";
+import Footer from "../components/Footer";
+import HeaderStudent from "../components/HeaderStudent";
 import styles from "../styles/Login.module.css";
-import _Head from "../components/_Head.js";
-import Header from "../components/Header.js";
-import Footer from "../components/Footer.js";
 
 export default function Sample() {
   return (
-    <div>
+    <>
       <_Head />
-      <div className={styles.container}>
-        <Header />
-        <main className={styles.main}>
-          <div id={styles.box}>
-            <h1 className={styles.title}>Thank You So Much!</h1>
-            <div>
-              We have received your submission and will reach out to your child
-              shortly. We greatly appreciate your interest.
-            </div>
-            <div>
-                In the meantime, please kindly see more about our website here <a href="https:/www.pairahealth.com">PAIRA</a>
-            </div>
-            <div>We hope you have a great day!</div>
-          </div>
-        </main>
-        <Footer />
-      </div>
-    </div>
+      <HeaderStudent />
+      <main className={styles.container + " " + styles.main}>
+        {/* Please kindly place text here */}
+      </main>
+      <Footer />
+    </>
   );
 }
+
+{/* <div id={styles.box}> */}
