09e4a14b1b2670d622698664cae50970bdc941a2
joeypark743 <90875941+joeypark743@users.noreply.github.com>
2022-08-11 08:26:00 -0700

    Students

---
+++ pages/mentor/students.js
@@ -0,0 +1,39 @@
+
+
+import Image from "next/image";
+import styles from "../../styles/StudentsTab.module.css";
+import commonStyles from "../../styles/common.module.css";
+import _Head from "../../components/_Head.js";
+import Footer from "../../components/Footer.js";
+import HeaderMentor from "../../components/HeaderMentor.js";
+import EditIcon from '@mui/icons-material/Edit';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Divider from "@mui/material/Divider";
+import TextField from "@mui/material/TextField";
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+
+export default function Students() {
+    return (
+        <><HeaderMentor />
+            <div className={commonStyles.container}>
+                <_Head />
+                <main className={commonStyles.main} style={{ paddingTop: "50px" }}>
+
+                    <div id={styles.mainBox}>
+                        
+
+                    </div>
+
+
+
+
+
+
+
+                </main>
+
+                <Footer />
+            </div ></>
+    );
+}
--- pages/student/insights.js
+++ pages/student/insights.js
@@ -1,32 +1,32 @@
-import _Head from "../../components/_Head";
-import Footer from "../../components/Footer";
-import HeaderStudent from "../../components/HeaderStudent";
-import InsightsDiscoverMoreInsights from "../../components/InsightsDiscoverMoreInsights";
-import InsightsMonthlyImprovements from "../../components/InsightsMonthlyImprovements";
-import InsightsPersonalityType from "../../components/InsightsPersonalityType";
-import InsightsTopStrengthsTopAreasOfGrowth from "../../components/InsightsTopStrengthsTopAreasOfGrowth";
-import styles from "../../styles/Login.module.css";
+// import _Head from "../../components/_Head";
+// import Footer from "../../components/Footer";
+// import HeaderStudent from "../../components/HeaderStudent";
+// import InsightsDiscoverMoreInsights from "../../components/InsightsDiscoverMoreInsights";
+// import InsightsMonthlyImprovements from "../../components/InsightsMonthlyImprovements";
+// import InsightsPersonalityType from "../../components/InsightsPersonalityType";
+// import InsightsTopStrengthsTopAreasOfGrowth from "../../components/InsightsTopStrengthsTopAreasOfGrowth";
+// import styles from "../../styles/Login.module.css";
 
-export default function Insights() {
-  return (
-    <>
-      <_Head />
-      <HeaderStudent />
-      {/* styles.main interfering with other styles? */}
-      <main className={styles.container}>
-        {/* First tab section */}
-        <div id={styles.box} style={{ display: "flex" }}>
-          {/* left side */}
-          <InsightsTopStrengthsTopAreasOfGrowth />
-          {/* right side */}
-          <InsightsPersonalityType />
-        </div>
-        {/* Second tab section */}
-        <InsightsMonthlyImprovements />
-        {/* Third tab section */}
-        <InsightsDiscoverMoreInsights />
-      </main>
-      <Footer />
-    </>
-  );
-}
+// export default function Insights() {
+//   return (
+//     <>
+//       <_Head />
+//       <HeaderStudent />
+//       {/* styles.main interfering with other styles? */}
+//       <main className={styles.container}>
+//         {/* First tab section */}
+//         <div id={styles.box} style={{ display: "flex" }}>
+//           {/* left side */}
+//           <InsightsTopStrengthsTopAreasOfGrowth />
+//           {/* right side */}
+//           <InsightsPersonalityType />
+//         </div>
+//         {/* Second tab section */}
+//         <InsightsMonthlyImprovements />
+//         {/* Third tab section */}
+//         <InsightsDiscoverMoreInsights />
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }
