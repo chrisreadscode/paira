ccab2f54c1ee3e5f0da0240bc73c38a9d974a183
Chris Read <24418909+rcreadii@users.noreply.github.com>
2022-08-11 08:54:11 -0400

    update head, header, and footer with link tags and dedicated stylesheet

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,19 @@
 import Image from "next/image";
-import Link from "next/Link";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link
-        href="/index"
-      >
-          <Image src="/paira-logo.png" alt="PAIRA Logo" width={100} height={19} />
+      <Link href="/index">
+        <a>
+          <Image
+            src="/paira-logo.png"
+            alt="PAIRA Logo"
+            width={100}
+            height={19}
+          />
+        </a>
       </Link>
     </footer>
   );
--- components/_Head.js
+++ components/_Head.js
@@ -1,23 +1,11 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
 
 export default function _Head() {
   return (
     <Head>
       <title>PAIRA</title>
       <meta name="description" content="Welcome to PAIRA!" />
-      <link rel="icon" href="/favicon.ico" />
-
-      {/* <div className={styles.header}>
-        <Image
-          alt="paira logo"
-          height="50px"
-          width="200px"
-          src="/paira-placeholder.png"
-          z-index="0"
-        />
-      </div> */}
+      {/* <link rel="icon" href="/favicon.ico" /> */}
     </Head>
   );
 }
