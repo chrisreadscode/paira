618eff22d540d5cd82ec8356192273bae99ece61
Chris Read <24418909+rcreadii@users.noreply.github.com>
2022-08-12 09:31:07 -0400

    add minor styling and name change functionality to student message page

--- pages/student/message.js
+++ pages/student/message.js
@@ -10,8 +10,13 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
 import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { ToggleButton } from "@mui/material";
+import {useState} from 'react';
+import { Button } from "@mui/material";
 
 export default function Message() {
+    const [recepient, setRecepient ] = useState({name: "Gina Wee", title: "Personal Mentor"});
+
     return (
         <><HeaderStudent />
             <div className={commonStyles.container}>
@@ -20,7 +25,8 @@ export default function Message() {
 
                     <div id={styles.mainBox}>
                         <div id={styles.mentorSide}>
-                            <div className={styles.mentorBox}>
+                            <div className={styles.mentorBox} onClick={(e) => setRecepient({name: "Gina Wee", title: "Personal Mentor"})}>
+                            {/* <ToggleButton style={{border: "0", padding: "0", margin: "0"}}> */}
                                 <Image alt="Profile Picture"
                                     src="/mentor-profile-pic.png"
                                     width="55px"
@@ -31,9 +37,11 @@ export default function Message() {
                                     <h3 className={styles.mentorNames}>Gina Wee</h3>
                                     <p className={styles.mentorNames} style={{ fontSize: "small", color: "#7c7c7c" }}> Personal Mentor</p>
                                 </div>
+                            {/* </ToggleButton> */}
                             </div>
 
-                            <div className={styles.mentorBox}>
+                            {/* <ToggleButton style={{border: "0", padding: "0", margin: "0", width: "100%"}}> */}
+                            <div className={styles.mentorBox} onClick={(e) => setRecepient({name: "Tina Richards", title: "Time Management Mentor"})}>
                                 <Image alt="Profile Picture"
                                     src="/mentor-profile-pic.png"
                                     width="55px"
@@ -45,8 +53,10 @@ export default function Message() {
                                     <p className={styles.mentorNames} style={{ fontSize: "small", color: "#7c7c7c" }}> Time Management <br></br> Mentor</p>
                                 </div>
                             </div>
+                            {/* </ToggleButton> */}
 
-                            <div className={styles.mentorBox}>
+                            {/* <ToggleButton style={{border: "0", padding: "0", margin: "0", width: "100%"}}> */}
+                            <div className={styles.mentorBox} onClick={(e) => setRecepient({name: "John Lake", title: "Sleep Mentor"})}>
                                 <Image alt="Profile Picture"
                                     src="/mentor-profile-pic.png"
                                     width="55px"
@@ -58,25 +68,32 @@ export default function Message() {
                                     <p className={styles.mentorNames} style={{ fontSize: "small", color: "#7c7c7c" }}> Sleep Mentor</p>
                                 </div>
                             </div>
+                            {/* </ToggleButton> */}
                         </div>
 
                         <div id={styles.messageSide}>
                             <div id={styles.messageTop}>
-                                <h3 className={styles.mentorNames} style={{ fontWeight: "heavy", fontSize: "x-large" }}> Gina Wee</h3>
-                                <p style={{ color: "#7c7c7c" }}> Personal Mentor</p>
+                                <h3 className={styles.mentorNames} style={{ fontWeight: "heavy", fontSize: "x-large" }}>{recepient.name}</h3>
+                                <p style={{ color: "#7c7c7c" }}>{recepient.title}</p>
                             </div>
                             <div id={styles.messageBottom}>
                                 <div style={{ height: "82%" }}>
 
                                 </div>
 
-                                <div style={{ height: "18%" }}>
-                                    <div id={styles.messageInput}>
-                                        <h3 id={styles.more}>+</h3>
-                                        <AttachFileIcon id={styles.attachFile} />
-                                        <TextField id="message" variant="standard" placeholder="Message..." sx={{ width: '70%' }} />
+                                <div style={{ display: "flex", alignItems: "flex-end", height: "18%", justifyContent: "flex-end"}}>
+                                    <div id={styles.messageInput} style={{display: "flex", margin: "0 1vw 1vh 0"}}>
+                                        <Button style={{alignItems: "center", display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                                            <h3 id={styles.more}>+</h3>
+                                        </Button>
+                                        <Button style={{display: "flex"}}>
+                                            <AttachFileIcon id={styles.attachFile} />
+                                        </Button>
+                                        <TextField id="message" variant="standard" placeholder="Message..." sx={{ width: '70%', paddingTop: "3px" }} />
 
-                                        <h4 style={{ margin: "auto 20px" }}>Send</h4>
+                                        <Button>
+                                            <h4 style={{ margin: "auto 20px" }}>Send</h4>
+                                        </Button>
                                     </div>
 
 
@@ -87,13 +104,6 @@ export default function Message() {
 
                         </div>
                     </div>
-
-
-
-
-
-
-
                 </main>
 
                 <Footer />
